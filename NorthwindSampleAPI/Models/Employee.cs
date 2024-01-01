06a6f244cef8c5cd90bac33453e53b3f02d83cee
Adam Surek <26888201+SuRektM8@users.noreply.github.com>
2023-12-31 22:56:54 -0500

    Created EmployeeDto to resolve object cycle. Added JsonIgnore attribute

--- NorthwindSampleAPI/Models/Employee.cs
+++ NorthwindSampleAPI/Models/Employee.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using Newtonsoft.Json;
+using NorthwindSampleAPI.Controllers;
 
 namespace NorthwindSampleAPI.Models;
 
@@ -41,11 +43,40 @@ public partial class Employee
 
     public string? PhotoPath { get; set; }
 
+    [JsonIgnore]
     public virtual ICollection<Employee> InverseReportsToNavigation { get; set; } = new List<Employee>();
 
+    [JsonIgnore]
     public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
 
+    [JsonIgnore]
     public virtual Employee? ReportsToNavigation { get; set; }
 
+    [JsonIgnore]
     public virtual ICollection<Territory> Territories { get; set; } = new List<Territory>();
+
+    public EmployeeDto GenerateDto()
+    {
+        return new EmployeeDto()
+        {
+            EmployeeId = this.EmployeeId,
+            FirstName = this.FirstName,
+            LastName = this.LastName,
+            Title = this.Title,
+            TitleOfCourtesy = this.TitleOfCourtesy,
+            BirthDate = this.BirthDate,
+            HireDate = this.HireDate,
+            Address = this.Address,
+            City = this.City,
+            Region = this.Region,
+            PostalCode = this.PostalCode,
+            Country = this.Country,
+            HomePhone = this.HomePhone,
+            Extension = this.Extension,
+            Photo = this.Photo,
+            Notes = this.Notes,
+            ReportsTo = this.ReportsTo,
+            PhotoPath = this.PhotoPath
+        };
+    }
 }
